9517e13343c9601d212ee1d19277738dd6122ab6
jamclaub <33885324+jamclaub@users.noreply.github.com>
2018-01-23 18:00:39 -0600

    random num guessing

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,7 +10,27 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Random arbitrarynum = new Random();
+            int number = arbitrarynum.Next(1, 100);
+            Console.Write("Im thinking about a number between one and one-hundred (take a guess): ");
+            int yourguess = int.Parse(Console.ReadLine());
+            int i = 0;
+            while(yourguess != number)
+            {
+                i++;
+                if (yourguess > number)
+                {
+                    Console.WriteLine("That is to high.");
+                }
+                else if(yourguess < number)
+                {
+                    Console.WriteLine("That is to low.");
+                }
+                Console.Write("guess again: ");
+                
+                yourguess = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("wooo you did it. It took you " + i + " trys to get it.");
             Console.ReadKey();
         }
     }
